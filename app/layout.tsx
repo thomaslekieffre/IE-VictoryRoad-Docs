122d5e38e370e440d70d3cc228b2e415c99bf7b9
thomaslekieffre <thomaslekieffre59.dev@gmail.com>
2025-11-26 18:26:49 +0100

    feat: new metadate

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,11 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Victory Road FR",
-  description: "Guides et outils pour optimiser Inazuma Eleven Victory Road.",
+  description: "Guides, stats et outils pour optimiser ton équipe sur Inazuma Eleven Victory Road. Builds de joueurs, routes de farm, docs techniques et ressources communautaires en français.",
   metadataBase: new URL("https://www.ie-victory-road.fr"),
   openGraph: {
     title: "Victory Road FR",
-    description: "Guides et outils pour optimiser Inazuma Eleven Victory Road.",
+    description: "Guides, stats et outils pour optimiser ton équipe sur Inazuma Eleven Victory Road. Builds de joueurs, routes de farm, docs techniques et ressources communautaires en français.",
     url: "https://www.ie-victory-road.fr",
     siteName: "Victory Road FR",
     images: [
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Victory Road FR",
-    description: "Guides et outils pour optimiser Inazuma Eleven Victory Road.",
+    description: "Guides, stats et outils pour optimiser ton équipe sur Inazuma Eleven Victory Road. Builds de joueurs, routes de farm, docs techniques et ressources communautaires en français.",
     images: [
       "https://media.discordapp.net/attachments/1440734138012536882/1443228200829456404/Inazuma-Eleven-Victory-Road-of-Heroes_2022_07-21-22_013.jpg?ex=69284e80&is=6926fd00&hm=0a65074f5da5063a6ae62236eee9be5cd9367133490d1e462f22e0d8e5017601&=&format=webp&width=1589&height=800",
     ],
